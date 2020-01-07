14ee33e0dfa79293fcc2ad7d833fc1e70b3980e6
Matthew LaForest <matthewalaforest@gmail.com>
2020-01-07 05:13:57 -0500

    :sparkles: (server) Handle routes other then index gracefully

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,6 +11,19 @@ const notifications = require("./notifications");
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static("dist"));
+
+app.get("*", function(request, response, next) {
+  if (request.headers.accept.includes("text/html")) {
+    const html = fs.readFileSync(path.resolve("dist/index.html"), "utf8");
+
+    response
+      .status(200)
+      .contentType("text/html")
+      .send(html);
+  } else {
+    next();
+  }
+});
 
 api.init(app);
 notifications.init();
